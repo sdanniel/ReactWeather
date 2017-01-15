1248818bf3e240dd7cc66bc2f846f3bd002f53a9
Daniel Spanov <daniel.s@taboola.com>
2017-01-15 23:36:14 +0200

    boilerplate

---
+++ app/app.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var firstName = 'Daniel';
+
+ReactDOM.render(
+  <h1>Hello Boilerplate!</h1>,
+  document.getElementById('app')
+);
