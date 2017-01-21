ca0055a17f5f83a8130cdb6e36ff610f37f78dd6
Daniel Spanov <daniel.s@taboola.com>
2017-01-22 00:42:56 +0200

    lala

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 8000 ;
 app.use(express.static('public'));
 
 app.listen(PORT, function() {
-  console.log('app is listening on port', PORT);
+  console.log('app is listening on port 1111', PORT);
 });
