250123c1e43dc15d3b4863e6049df27cba1da67e
Daniel Spanov <daniel.s@taboola.com>
2017-02-04 22:51:55 +0200

    add examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return (
       <div>
-        <h3>Examples</h3>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are few examples to try out:</p>
+        <ol>
+          <li>
+            <Link to="/?location=Ramat-Gan">Ramat-Gan</Link>
+          </li>
+          <li>
+            <Link to="/?location=Moscow">Moscow</Link>
+          </li>
+        </ol>
       </div>
     );
 };
