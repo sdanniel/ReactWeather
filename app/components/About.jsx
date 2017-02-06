0e34ca9706de47cf2049a313906fd5b0374e631e
Daniel Spanov <daniel.s@taboola.com>
2017-02-06 23:53:14 +0200

    about

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,7 +1,12 @@
 var React = require('react');
 
 var About = (props) => {
-  return <h3>About Component</h3>;
+  return (
+    <div>
+      <h1 className="text-center">About</h1>
+      <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTzE5eOGhRgqKJC7mEPrQreqxXBDnHL5SZ2IWoeUmdsGa23vyCL"></img>
+    </div>
+  );
 };
 
 module.exports = About;
